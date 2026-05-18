911c60c832907d9bda233edd7538437bbfd7451f
MaryamAlarifi <2026260@student.cct.ie>
2026-05-18 12:47:30 +0100

    Update Uber.py

--- Uber.py
+++ Uber.py
@@ -19,3 +19,8 @@ data=load_data(10000)
 data_load_state=st.text("Loding data done!")
 st.subheader("Row Data")
 st.write(data)
+
+
+st.subheader("number of pick ups")
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour,bins=24,range=(0,24))[0]
+st.bar_chart(hist_values)
